e53bcdc212ac05aa6bbb55841833acf567a2382e
hcastillo <hcastillo@lsv-tech.com>
2025-08-24 00:02:33 -0500

    V0.1 Initial project

---
+++ applications/app-service/src/main/java/bootcamp/reto/poweup/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package bootcamp.reto.poweup.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "bootcamp.reto.poweup.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
